22f827e8f2a42ebc1166e754f88acd3aac23690c
masiceiu <masiceiu@gmail.com>
2021-08-07 15:49:42 +0600

    2 files modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line
   selector: 'body',
-  template: '<router-outlet></router-outlet>'
+  template: 'a'
 })
 export class AppComponent {
   ngOnInit() {
     //temp fix because stackblitz doesn't allow access to index.html
-    document.body.className = 'app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden';
+    //document.body.className = 'app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden';
   }
 }
